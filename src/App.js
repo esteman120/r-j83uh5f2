df01faf11c914620411a2672289a1b6bfa7b3032
unknown <estebanbotero1036@gmail.com>
2019-02-14 14:02:20 -0500

    d

--- src/App.js
+++ src/App.js
@@ -2,13 +2,30 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      numeroCaracteres: 0,
+      valor:""
+    }
+  }
+
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={this.contador.bind(this)} value={this.state.valor}></textarea>
+        <div className="counter">{this.state.numeroCaracteres}</div>
       </div>
     );
+  }
+
+  contador(event){
+      let numeroCaracteres = event.target.value.length;
+      this.setState({
+        valor: event.target.value,
+        numeroCaracteres: numeroCaracteres
+      });
   }
 }
 
